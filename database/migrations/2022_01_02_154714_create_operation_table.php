4839d27f0814ba26c4ef8ebad4366807ce60cd4f
大田 <>
2022-01-04 23:57:53 +0900

    first-commit

---
+++ database/migrations/2022_01_02_154714_create_operation_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOperationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operation', function (Blueprint $table) {
+            $table->bigInteger('id')->nullable();
+            $table->VARCHAR('content',191)->nullable();
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operation');
+    }
+}
